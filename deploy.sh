40fa2e22d413ed91407b575fdc82a0c9fa21e69f
tonz <mcteamster@gmail.com>
2024-09-28 18:58:22 +1000

    feat: upgrade to vite

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ cd "./twinge-service"
 serverless deploy
 cd "../twinge-client"
 npm run build
-aws s3 sync "./build" "s3://twinge-client"
+aws s3 sync "./dist" "s3://twinge-client"
 cd ..
 aws cloudfront create-invalidation --distribution-id="ERFLEM1FN5M32" --paths="/*"
