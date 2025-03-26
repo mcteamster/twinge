904bb18ed6e3bcb372826583615fd7fc81d25a4e
tonz <mcteamster@gmail.com>
2025-03-26 20:39:55 +1100

    feat: add clarity

--- twinge-client/src/index.jsx
+++ twinge-client/src/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Clarity from '@microsoft/clarity';
+
+// Initialize Clarity
+Clarity.init('qu6uzrb8ru');
 
 ReactDOM.render(
   <React.StrictMode>
